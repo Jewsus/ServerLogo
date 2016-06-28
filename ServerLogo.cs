3dc1b525ee4e577adb312555f8cfeefc15a9cd38
Jewsus <hismoses@hotmail.com>
2016-06-28 18:12:03 +1000

    API 1.23

---
+++ ServerLogo.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Terraria;
+using TerrariaApi.Server;
+using System.IO;
+using TShockAPI;
+
+namespace ServerLogo
+
+{
+    [ApiVersion(1, 23)]
+    public class ServerLogo : TerrariaPlugin
+    {
+        public static ServerLogoConfigFile ServerLogoConfig { get; set; }
+        internal static string ServerLogoConfigPath { get { return Path.Combine(TShock.SavePath, "ServerLogoconfig.json"); } }
+        public override string Author
+        {
+            get
+            {
+                return "Jewsus";
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Provides useless but shiny Server Logo";
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "ServerLogo";
+            }
+        }
+
+        public override Version Version
+        {
+            get
+            {
+                return new Version("1.0");
+            }
+        }
+
+        public ServerLogo(Main Game) : base(Game)
+        {
+            Order = -50;
+            ServerLogoConfig = new ServerLogoConfigFile();
+        }
+
+        public override void Initialize()
+        {
+            SetupConfig();
+            PrintIntro();
+
+            ServerApi.Hooks.GameInitialize.Register(this, (init) =>
+            {
+
+            });
+        }
+
+        public static void SetupConfig()
+        {
+            try
+            {
+                if (File.Exists(ServerLogoConfigPath))
+                {
+                    ServerLogoConfig = ServerLogoConfigFile.Read(ServerLogoConfigPath);
+                }
+                ServerLogoConfig.Write(ServerLogoConfigPath);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error in ServerLogo config file");
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                TShock.Log.ConsoleError("ServerLogo Config Exception");
+                TShock.Log.ConsoleError(ex.ToString());
+            }
+        }
+
+        protected void PrintIntro()
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo1);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo2);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo3);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo4);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo5);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo6);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo7);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo8);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo9);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo10);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo11);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo12);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(ServerLogoConfig.logo13);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("\r\n");
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(" Copyright (C) Jewsus, 2016 - ");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("http://geck-forums.com");
+
+            Console.WriteLine("\r\n");
+        }
+    }
+}
---
+++ ServerLogoConfig.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ServerLogo
+{
+    public class ServerLogoConfigFile
+    {
+        public string logo1 = " █░░▒▓▓▒░░░░░▒▓▀▄▀▄▀▄▓▒░░░░░▒▓▓▒░░█ ";
+        public string logo2 = " █                                █ ";
+        public string logo3 = " █              ╔╗   ╔═══╗  ╔═══╗ █ ";
+        public string logo4 = " █              ║║   ║╔═╗║  ║╔═╗║ █ ";
+        public string logo5 = " █ ╔══╗╔═══╗╔══╗║║╔╗ ╚╝╔╝║  ║║║║║ █ ";
+        public string logo6 = " █ ║╔╗║╠══║║║╔═╝║╚╝╝ ╔═╝╔╝  ║║║║║ █ ";
+        public string logo7 = " █ ║╚╝║║║══╣║╚═╗║╔╗╗ ║║╚═╗╔╗║╚═╝║ █ ";
+        public string logo8 = " █ ╚═╗║╚═══╝╚══╝╚╝╚╝ ╚═══╝╚╝╚═══╝ █ ";
+        public string logo9 = " █ ╔═╝║╔╗ ┬ ┬   ╦┌─┐┬ ┬┌─┐┬ ┬┌─┐  █ ";
+        public string logo10 = " █ ╚══╝╠╩╗└┬┘   ║├┤ │││└─┐│ │└─┐  █ ";
+        public string logo11 = " █     ╚═╝ ┴   ╚╝└─┘└┴┘└─┘└─┘└─┘  █ ";
+        public string logo12 = " █                                █ ";
+        public string logo13 = " █░░▒▓▓▒░░░░░▒▓▀▄▀▄▀▄▓▒░░░░░▒▓▓▒░░█ ";
+
+
+        public static ServerLogoConfigFile Read(string path)
+        {
+            if (!File.Exists(path))
+                return new ServerLogoConfigFile();
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return Read(fs);
+            }
+        }
+        public static ServerLogoConfigFile Read(Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                var cf = JsonConvert.DeserializeObject<ServerLogoConfigFile>(sr.ReadToEnd());
+                if (ConfigRead != null)
+                    ConfigRead(cf);
+                return cf;
+            }
+        }
+        public void Write(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                Write(fs);
+            }
+        }
+        public void Write(Stream stream)
+        {
+            var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+            using (var sw = new StreamWriter(stream))
+            {
+                sw.Write(str);
+            }
+        }
+        public static Action<ServerLogoConfigFile> ConfigRead;
+    }
+}
